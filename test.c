98ebd7a31bf43b66fda3faed0cc556a942621e82
Huskarl <gpavlun@clemson.edu>
2026-01-20 08:30:11 -0500

    mous

--- test.c
+++ test.c
@@ -9,100 +9,92 @@
 #include <panel.h>       // panel support for ncurses
 #include <menu.h>        // menu support for ncurses
 
-int wstrlen(int *buffer);
-int wstrcpy(int *dest,int *source);
 
-void main(void){
+#include <fcntl.h>
+#include <linux/input.h>
 
-    setlocale(LC_ALL, "");
+#include <errno.h>
 
 
-    initscr();          // Initialize the curses library
-    noecho();           // Don't echo user input to screen
-    raw();              // Disable line buffering
-    keypad(stdscr, TRUE); // Enable special keys (arrows, etc)
-    curs_set(0);        // Hide the cursor
-    refresh();  
-    
-    int maxr, maxc;
-    getmaxyx(stdscr, maxr, maxc);
-    int buffer[512];
-    int pbuffer[512];
-    FILE *BackgroundFile = fopen("background.txt","r");
-    int i,j,k,l,m; 
-    unsigned int r, c, size;
+int main(void)
+{
+    setlocale(LC_ALL, "");  
+    initscr();              
+    noecho();               
+    raw();                  
+    keypad(stdscr, TRUE);   
+    curs_set(0);
 
-    fgetws(buffer,512,BackgroundFile);
-    j = 0;
-    while(buffer[j]!='\n'){
-        j++;
+    unsigned int MaxRows,MaxCols;
+    getmaxyx(stdscr,MaxRows,MaxCols);
+    float x = MaxCols/2;
+    float y = MaxRows/2;
+    float tempev;
+    float oldx = x;
+    float oldy = y;
+
+    int fd = open("/dev/input/event6", O_RDONLY);
+    if (fd < 0) {
+        perror("open");
+        return 1;
     }
-    buffer[j+1] = 0;
-                
-    int len = j;
-    fclose(BackgroundFile);
-    while(1){
-        for(l=0;l<len;l++){
-            getmaxyx(stdscr, maxr, maxc);
-            i=0;
-            BackgroundFile = fopen("background.txt","r");
-            for(i=0;i<21;i++){
-                fgetws(pbuffer,512,BackgroundFile);
-                j = 0;
-                k = 0;
-                
-                while(pbuffer[j]!='\n'){
-                    j++;
-                }
-                pbuffer[j+1] = 0;
-        
-                wstrcpy(&(buffer[0]),&(pbuffer[l]));
-                
-                if(wstrlen(buffer)>maxc){
-                    mvaddnwstr(i,0,buffer,maxc);
-                }else{
-                    if(wstrlen(buffer)<maxc){
-                        m = 0;
 
-                        mvaddwstr(i, 0, buffer);
+    struct input_event ev;
 
-                        j = maxc;
-                        j -= wstrlen(buffer);
-                        if(j-wstrlen(pbuffer)>0){
-                            mvaddwstr(i, (wstrlen(buffer)-1) + (wstrlen(pbuffer)-1)*m, pbuffer);
-                            m++;
-                            j -= wstrlen(buffer);
-                        }
-                        mvprintw(1,1,"%d",wstrlen(buffer)-1);
-                        if(maxc-((wstrlen(buffer)-1) + (wstrlen(pbuffer)-1)*m)!=0){
-                            mvaddnwstr(i, (wstrlen(buffer)-1) + (wstrlen(pbuffer)-1)*(m), pbuffer, maxc - (wstrlen(buffer)-1) - (wstrlen(pbuffer)-1)*(m));
-                        }
-                    }else{
-                        mvaddnwstr(i, 0, buffer, maxc);
-                    }
-                }
-            } 
-            fclose(BackgroundFile);
-            refresh();  
-            usleep(125000);
+    while (1) {
+        read(fd, &ev, sizeof(ev));
+
+        /* Mouse movement */
+        if (ev.type == EV_REL && ev.code == REL_X){
+            mvprintw(2,1,"dx = %d\n", ev.value);
+            tempev = ev.value;
+            
+            x+=tempev/6;
+            
+            
         }
-    }
-    endwin();
-}
+        if (ev.type == EV_REL && ev.code == REL_Y){
+            mvprintw(3,1,"dy = %d\n", ev.value);
+            tempev = ev.value;
+            
+            y+=tempev/6;
+        }
 
-int wstrlen(int *buffer){
-    int i = 0;
-    while(buffer[i]!=0){
-        i++;
+        mvprintw(1,1,"(%f,%f)", y,x);
+
+
+
+        if(y<0){
+            y=0;
+        }else if(y>=MaxRows){
+            y=MaxRows-1;
+        }
+        if(x<0){
+            x=0;
+        }else if(x>=MaxCols){
+            x=MaxCols-1;
+        }
+        //clear();
+        mvprintw(oldy,oldx," ");
+        mvprintw(y,x,"+");
+        oldx = x;
+        oldy = y;
+        
+
+        /* Mouse clicks */
+        if (ev.type == EV_KEY) {
+            if (ev.code == BTN_LEFT){
+                mvprintw(3,1,"Left %s\n", ev.value ? "down" : "up");
+                goto end;
+            }
+            if (ev.code == BTN_RIGHT)
+                mvprintw(4,1,"Right %s\n", ev.value ? "down" : "up");
+
+            if (ev.code == BTN_MIDDLE)
+                mvprintw(5,1,"Middle %s\n", ev.value ? "down" : "up");
+        }
+        refresh();
     }
-    return i;
-}
-int wstrcpy(int *dest,int *source){
-    int i = 0;
-    while(source[i]!=0){
-        dest[i] = source[i];
-        i++;
-    }
-    dest[i] = 0; 
-    return 0;
+    end:
+    endwin();
 }
